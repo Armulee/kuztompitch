f98d942639a4b1c8f7f18369a758fb9ecdb1c082
Armulee <makkaroon.thanawat@gmail.com>
2025-05-31 17:01:42 +0700

    Alpha Version

---
+++ app/api/submit-order/route.ts
@@ -0,0 +1,30 @@
+import { NextResponse } from "next/server"
+
+export async function POST(req: Request) {
+    const data = await req.json()
+    const url =
+        "https://script.google.com/macros/s/REDACTED/exec"
+
+    try {
+        const response = await fetch(url, {
+            method: "POST",
+            headers: {
+                "Content-Type": "application/json",
+            },
+            body: JSON.stringify(data),
+        })
+
+        const result = await response.json()
+        if (result.success) {
+            return NextResponse.json(result, { status: 200 })
+        } else {
+            return NextResponse.json({ message: "Error" }, { status: 400 })
+        }
+    } catch (err) {
+        console.error(err)
+        return NextResponse.json(
+            { message: "Internal Server Error" },
+            { status: 500 }
+        )
+    }
+}
