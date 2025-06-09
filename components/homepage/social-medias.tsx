fc3a14585b29cac4fd3d2a5244850d25b71f4db4
Armulee <makkaroon.thanawat@gmail.com>
2025-06-09 18:42:16 +0700

    Beta Version

--- components/homepage/social-medias.tsx
+++ components/homepage/social-medias.tsx
@@ -1,12 +1,29 @@
-import { FaFacebook, FaInstagram, FaTiktok, FaXTwitter } from "react-icons/fa6"
+import { FaFacebook, FaInstagram, FaLine, FaTiktok } from "react-icons/fa6"
 
 const SocialMedias = ({ className }: { className?: string }) => {
     return (
         <div className={`${className} flex items-center gap-5`}>
-            <FaFacebook className='w-5 h-auto' />
-            <FaInstagram className='w-5 h-auto' />
-            <FaXTwitter className='w-5 h-auto' />
-            <FaTiktok className='w-5 h-auto' />
+            <a
+                href='https://www.facebook.com/share/1ApdBRs7wV/?mibextid=wwXIfr'
+                target='_blank'
+            >
+                <FaFacebook className='w-5 h-auto' />
+            </a>
+            <a
+                href='https://www.instagram.com/kuztompitch?igsh=MTQxbHM2Y3BpcTNndA%3D%3D&utm_source=qr'
+                target='_blank'
+            >
+                <FaInstagram className='w-5 h-auto' />
+            </a>
+            <a href='https://lin.ee/AvwUPdl' target='_blank'>
+                <FaLine className='w-5 h-auto' />
+            </a>
+            <a
+                href='https://www.tiktok.com/@kuztompitch?_t=ZS-8x3lkEv1byk&_r=1'
+                target='_blank'
+            >
+                <FaTiktok className='w-5 h-auto' />
+            </a>
         </div>
     )
 }
